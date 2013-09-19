13f540c2c58a492be80306313b3ebf70d1ae01e0
DaleSwanson <crazysteve138@hotmail.com>
2013-09-19 19:42:45 -0400

    Initial Commit

---
+++ njcities.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+#requires lynx
+
+use strict;
+use warnings;
+use autodie;
+
+#$|++; #autoflush disk buffer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print "\nDone\n\n";
